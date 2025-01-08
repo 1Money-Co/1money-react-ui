a2e9c88df932c969f709232450d907923bfe9f48
Bobby Talvin <bobby@1money.com>
2025-01-08 17:52:36 +0800

    update: Drawer UI

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react';
 import propTypes from 'prop-types';
 import { Sidebar } from 'primereact/sidebar';
+import { Icons } from '@/components/Icons';
 import classnames from '@/utils/classnames';
 /* import types */
 import type { FC, PropsWithChildren } from 'react';
@@ -13,6 +14,14 @@ export const Drawer: FC<PropsWithChildren<DrawerProps>> = props => {
   return (
     <Sidebar
       className={classes(void 0, className)}
+      icons={
+        <Icons name='logoWithWords' logoColor='#073387' width={157} height={30} />
+      }
+      closeIcon={
+        <i className={classes('close')}>
+          <Icons name='times' size={24} />
+        </i>
+      }
       {...rest}
     >
       { children }
