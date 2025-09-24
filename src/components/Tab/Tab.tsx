5f28748903b2f63a40f2b1f3d7e590426d5770ff
wali-1money <wali@1money.com>
2025-09-24 14:08:58 +0800

    update table style and input style

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -6,7 +6,7 @@ import type { FC, PropsWithChildren } from 'react';
 import type { TabProps } from './interface';
 
 export const Tab: FC<PropsWithChildren<TabProps>> = props => {
-  const { className, prefixCls = 'tab', model, type = 'default', ...rest } = props;
+  const { className, lableClassName, prefixCls = 'tab', model, type = 'default', ...rest } = props;
   const classes = classnames(prefixCls);
 
   return (
@@ -19,7 +19,7 @@ export const Tab: FC<PropsWithChildren<TabProps>> = props => {
             template: (_, options) => {
               const { className, labelClassName, iconClassName, onClick } = options;
               return (
-                <div className={classes('item', className)} onClick={onClick}>
+                <div className={[classes('item', className), lableClassName].join(' ').trim()} onClick={onClick}>
                   { item.icon != null && <span className={iconClassName}>{ item.icon }</span> }
                   { item.label != null && <strong className={classes('item-label', labelClassName)}>{ item.label }</strong> }
                   { item.count != null && <span className={classes('item-count')}>{ item.count }</span> }
