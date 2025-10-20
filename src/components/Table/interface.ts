cf8ac856895a0e214833665a0f71add77afa7821
Bobby Talvin <bobby@1money.com>
2025-10-20 14:47:32 +0800

    upgrade: primereact & Table types adapt

--- src/components/Table/interface.ts
+++ src/components/Table/interface.ts
@@ -1,5 +1,5 @@
 import type { RefObject } from 'react';
-import type { DataTable, DataTableBaseProps, DataTableValueArray, DataTablePropsSingle, DataTablePropsMultiple, DataTablePropsCell } from 'primereact/datatable';
+import type { DataTable, DataTableBaseProps, DataTableValueArray, DataTablePropsSingle, DataTablePropsMultiple, DataTablePropsCellSingle, DataTablePropsCellMultiple } from 'primereact/datatable';
 import type { ColumnProps } from 'primereact/column';
 
 export type TableClass<T extends DataTableValueArray = DataTableValueArray> = DataTable<T>;
@@ -18,6 +18,8 @@ export interface TableSingleProps<T extends DataTableValueArray = DataTableValue
 
 export interface TableMultipleProps<T extends DataTableValueArray = DataTableValueArray> extends DataTablePropsMultiple<T>, TableBaseProps<T> {}
 
-export interface TableCellProps<T extends DataTableValueArray = DataTableValueArray> extends DataTablePropsCell<T>, TableBaseProps<T> {}
+export interface TableCellSingleProps<T extends DataTableValueArray = DataTableValueArray> extends DataTablePropsCellSingle<T>, TableBaseProps<T> {}
 
-export type TableProps<T extends DataTableValueArray = DataTableValueArray> = TableSingleProps<T> | TableMultipleProps<T> | TableCellProps<T>;
+export interface TableCellMultipleProps<T extends DataTableValueArray = DataTableValueArray> extends DataTablePropsCellMultiple<T>, TableBaseProps<T> {}
+
+export type TableProps<T extends DataTableValueArray = DataTableValueArray> = TableSingleProps<T> | TableMultipleProps<T> | TableMultipleProps<T> | TableCellMultipleProps<T>;
